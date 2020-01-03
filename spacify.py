9be0f6b2c2b4ad740446009b12efba01e36684d3
qwertpi <qwertpi@users.noreply.github.com>
2020-01-03 17:51:27 +0000

    Code rewrite
    The algorithm is hopefully a bit clearer, and new pre and post-processing stages result in better output and increased execution speed. Testing has shown a more restricted word list which only contains words relevant to the context of the input text produces better output but as this depends on the use case this has not resulted in changes to the repo

--- spacify.py
+++ spacify.py
@@ -1,61 +1,109 @@
 import re
+import string
 
-print("Loading")
-#load all the words in from the text file
 #splitlines instead of readlines as splitlines removes newline chars
-words=open("words.txt","r").read().splitlines()
-#gets the text to be spaced removes any existing spaces and makes it all lower case
-text=input("    ").replace(" ","").lower()
-#strips characters that aren't a-z commas full stops exclemation marks or question marks from the text
-text=re.sub("[^.,!a-zA-Z]","",text)
+#all printable charcters are added to prevent the code from getting stuck if no words exist
+words = open("words.txt","r").read().splitlines() + list(string.printable)
 
-spaced_text=""
-current_attempt=""
-previous_attempt=""
-#change this to make the code print every word it considers
-debug=False
+try:
+    #loads from file
+    #merges onto one line
+    inp = "".join(open("input.txt").read().splitlines())
+#unless no files exist
+except FileNotFoundError:
+    #in which case loads from input
+    inp = input(">  ")
 
-#while there is any unspaced text
-while len(text)!=0:
-    for char in text:
-        #add to the current attempt one char at a time
-        current_attempt+=char
-        #makes a list of all words starting with the text in current attempt
-        regex = re.compile(current_attempt)
-        matches = [word for word in words if regex.match(word)]
-        #if there are no matching words end the loop
-        if len(matches)==0:
-            break
+#removes all characters from input that aren't letters, or certain punctuation
+banned_chars = "".join(list(set(string.printable) - set(string.ascii_letters) - set([",", "!", ".", "?", "-"])))
+for char in banned_chars:
+    inp = inp.replace(char, "")    
 
-        try:
-            #check if the current attempt is an actual word
-            #if it isn't we won't get anyfurther with the text in this try block
-            words.index(current_attempt)
-            #debug infp
-            if debug:
-                print("Trying",current_attempt)
-            #set previous attempt to the current_attempt
-            previous_attempt=current_attempt
-        #prevents the program from stopping execution due to an Index "Error" if the current attempt isn't a word
-        except ValueError:
-            pass
-        #if we have built up the entire of the text as current attempt
-        if len(current_attempt)==len(text):
-            #break as we can't go anyfurther
-            break
-    #add the previous_attempt (the last attempt to be an actual word) to spaced text with a space after it
-    spaced_text+=previous_attempt+" "
-    #remove the spaces that are added before punctuation
-    spaced_text=spaced_text.replace(" ,",",")
-    spaced_text=spaced_text.replace(" .",".")
-    spaced_text=spaced_text.replace(" !","!")
-    spaced_text=spaced_text.replace(" ?","?")
-    #remove the text we just added to spaced_text from text
-    text=text[len(previous_attempt):]
-    
-    #resets current_attempt and previous_attempt
-    current_attempt=""
-    previous_attempt=""
-    #print the spaced text so far
-    print(spaced_text)
-print(spaced_text)
+char = ""
+prev_char = ""
+out = ""
+
+#rule based spacification to speed up the main brute force spacification
+for char in inp:
+    #if the current character is uppercase and the previous character wasn't uppercase
+    #as captials require spaces with the exception of acronyms
+    #or if the character is punctuation
+    if (char in string.ascii_uppercase and prev_char not in string.ascii_uppercase) or char not in list(string.ascii_letters):
+        #adds a space before the next character
+        out += " "
+    out += char
+    #pucntuation isn't included in prev_char
+    if char in string.ascii_letters:
+        prev_char = char
+
+print(out)
+
+longest_word = ""
+word_segment = ""
+inp = out
+out = ""
+
+def possible_words(string, words):
+    '''
+    Tells you whether any words begin with a certain string
+    :param string: str, the string you want to check for words begining with
+    :param words: list, the words you want to check for matches in
+    :returns: bool, whether 
+    '''
+    #cehcks every word
+    for word in words:
+        regex = re.compile("^" + string)
+        #if the word starts with with string
+        if bool(regex.search(word)):
+            #return causes the function to quit
+            return True
+    #this will only be ran if we get to the end of the for loop without having returned True
+    return False
+
+#respects the word bounadries created by the rules
+for word in inp.split(" "):
+    while len(word) != 0:
+        for char in word:
+            word_segment += char.lower()
+            #if no words start with the word segment
+            if not possible_words:
+                #we've already found the longest possible word so breaks out of the for char loop
+                break
+            #if word_segment is a word
+            try:
+                words.index(word_segment)
+                longest_word = word_segment
+            #else do nothing
+            except ValueError:
+                pass
+        #once we've either reached the end of the for char loop or (more likely) no words starting with word_segment exist
+        #adds the longest word we found to the output string followed by a space
+        out += longest_word + " "
+        #remove the word we found from the input
+        #else the while len() > 0 will never end
+        word = word[len(longest_word):]
+        #resets relevant variables ready to find the next word
+        longest_word = ""
+        word_segment = ""
+        #very basic progress indicator
+        #without flush=True the output won't be shown until a new line occurs
+        print("_", end="", flush=True)
+
+print("")
+print(out)
+
+inp = out
+out = ""
+
+prev_char = ""
+for char in inp:
+    #strips spaces from before punctuation
+    if char not in string.punctuation or prev_char != " ":
+        out += prev_char
+    prev_char = char
+
+#provides final output
+print(out)
+#also sticks it in a text file for increased conveinece
+with open("out.txt", "w") as f:
+    f.write(out)
